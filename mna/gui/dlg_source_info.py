81c203148e50613d2221e6ffdeee63b7a1943a11
Karol Będkowski <karol.bedkowski@gmail.com>
2015-01-11 15:16:51 +0100

    fix dlg_source_info; add articles count

--- mna/gui/dlg_source_info.py
+++ mna/gui/dlg_source_info.py
@@ -17,6 +17,7 @@ from PyQt4 import QtGui, QtCore
 
 from mna.gui import resources_rc
 from mna.gui import dlg_source_info_ui
+from mna.model import dbobjects as DBO
 
 _LOG = logging.getLogger(__name__)
 
@@ -29,7 +30,7 @@ class DlgSourceInfo(QtGui.QDialog):
     def __init__(self, parent, source):
         _LOG.info("DlgSourceInfo.init: %r", source)
         QtGui.QDialog.__init__(self, parent)
-        self._ui = dlg_source_info_ui.Ui_DialogSourceInfo()
+        self._ui = dlg_source_info_ui.Ui_DlgSourceInfo()
         self._ui.setupUi(self)
         self._setup(source)
         self._bind()
@@ -40,18 +41,21 @@ class DlgSourceInfo(QtGui.QDialog):
         self._create_logs_model(source)
 
     def _create_info_model(self, source):
+        articles_cnt = DBO.Article.count(source_id=source.oid)
         info = [('Name', source.name),
                 ('Title', source.title),
                 ('Last refreshed', unicode(source.last_refreshed)),
                 ('Next refresh', unicode(source.next_refresh)),
                 ('Last error date', unicode(source.last_error_date)),
-                ('Last error', unicode(source.last_error))]
+                ('Last error', unicode(source.last_error)),
+                ('Articles', unicode(articles_cnt))]
 
         model = QtGui.QStandardItemModel(0, 2, self._ui.lv_info)
         model.setHeaderData(0, QtCore.Qt.Horizontal, self.tr("Key"))
         model.setHeaderData(1, QtCore.Qt.Horizontal, self.tr("Value"))
         for key, val in info:
-            model.appendRow([QtGui.QStandardItem(key), QtGui.QStandardItem(val)])
+            model.appendRow([QtGui.QStandardItem(key),
+                             QtGui.QStandardItem(val)])
         self._ui.lv_info.setModel(model)
         self._ui.lv_info.resizeColumnToContents(0)
         self._ui.lv_info.resizeColumnToContents(1)
