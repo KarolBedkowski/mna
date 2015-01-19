4f23d0686fcfe2974cb4fde1cd1746a451d29e5a
Karol Będkowski <karol.bedkowski@gmail.com>
2015-01-19 20:10:41 +0100

    + DlgFilterType, DlgFilterOptions

---
+++ mna/gui/filter_conf.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+""" Filters configuration gui.
+
+Copyright (c) Karol Będkowski, 2015
+
+This file is part of mna
+Licence: GPLv2+
+"""
+
+__author__ = "Karol Będkowski"
+__copyright__ = "Copyright (c) Karol Będkowski, 2015"
+__version__ = "2015-01-19"
+
+import logging
+
+from PyQt4 import QtGui
+
+from mna import plugins
+from mna.model import dbobjects as DBO
+from mna.gui import dlg_filter_type_ui
+from mna.gui import dlg_filter_options_ui
+
+_LOG = logging.getLogger(__name__)
+
+
+class DlgFilterType(QtGui.QDialog):
+    def __init__(self, parent=None, value=None):
+        QtGui.QDialog.__init__(self, parent)
+        self._ui = dlg_filter_type_ui.Ui_FilterTypeDialog()
+        self._ui.setupUi(self)
+        for name, fltr in plugins.FILTERS.iteritems():
+            self._ui.cb_filter.addItem(fltr.name, name)
+        if value:
+            idx = self._ui.cb_filter.findData(value)
+            assert idx, 'value %r not found in filters' % value
+            self._ui.cb_filter.setCurrentIndex(idx)
+        self.value = value
+
+    def done(self, result):
+        if result == QtGui.QDialog.Accepted:
+            self.value = self._ui.cb_filter.itemData(
+                self._ui.cb_filter.currentIndex()).toString()
+        return QtGui.QDialog.done(self, result)
+
+
+class DlgFilterOptions(QtGui.QDialog):
+    def __init__(self, parent, fltr):
+        QtGui.QDialog.__init__(self, parent)
+        self.fltr = fltr
+        self._ui = dlg_filter_options_ui.Ui_DlgFilterOptions()
+        self._ui.setupUi(self)
+
+        self._widgets = {}
+        lay = self._ui.fl_options
+        values = fltr.cfg.conf
+        for name, opts in fltr.get_params().iteritems():
+            title, ftype, default = opts
+            lay.addWidget(QtGui.QLabel(title))
+            value = values.get(name) if values else None
+            if value is None:
+                value = default
+            if ftype == int:
+                wdg = QtGui.QSpinBox()
+                wdg.setValue(value or 0)
+            elif ftype == str:
+                wdg = QtGui.QLineEdit()
+                wdg.setText(value or '')
+            else:
+                raise RuntimeError('invalid field type %r - %r', name, opts)
+            lay.addWidget(wdg)
+            self._widgets[name] = wdg
+
+    def done(self, result):
+        if result == QtGui.QDialog.Accepted:
+            fltr = self.fltr
+            for name, opts in fltr.get_params().iteritems():
+                _title, ftype, value = opts
+                wdg = self._widgets[name]
+                if ftype == int:
+                    value = wdg.value()
+                elif ftype == str:
+                    value = wdg.text()
+                self.fltr.cfg.conf[name] = value
+        return QtGui.QDialog.done(self, result)
+
+
+def add_filter(parent_wnd, source_id):
+    dlg = DlgFilterType(parent_wnd)
+    if dlg.exec_() != QtGui.QDialog.Accepted:
+        return
+    fltr_cfg = DBO.Filter()
+    fltr_cfg.source_id = source_id
+    fltr = plugins.FILTERS[dlg.value](fltr_cfg)
+    dlg = DlgFilterOptions(parent_wnd, fltr)
+    if dlg.exec_() != QtGui.QDialog.Accepted:
+        fltr_cfg.save()
+        return True
+    return False
