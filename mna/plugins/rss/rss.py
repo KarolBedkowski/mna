53c20ba35e9a5e0c9f56ce5728952915eeec8cab
Karol Będkowski <karol.bedkowski@gmail.com>
2015-02-08 11:38:30 +0100

    rss: fixes and cleanup

--- mna/plugins/rss/rss.py
+++ mna/plugins/rss/rss.py
@@ -25,11 +25,11 @@ from . import frm_sett_rss_ui
 _LOG = logging.getLogger(__name__)
 
 
-def _ts2datetime(tstruct):
+def _ts2datetime(tstruct, default=None):
     """ Convert time stucture to datetime.datetime """
     if tstruct:
         return datetime.datetime.fromtimestamp(time.mktime(tstruct))
-    return None
+    return default
 
 
 class FrmSettRss(QtGui.QFrame):  # pylint: disable=no-member
@@ -66,6 +66,7 @@ class RssSource(base.AbstractSource):
     def get_items(self, session=None, max_load=-1, max_age_load=-1):
         _LOG.info("RssSource: src=%d get_items", self.cfg.oid)
         url = self.cfg.conf.get("url") if self.cfg.conf else None
+        now = datetime.datetime.now()
         if not url:
             return []
 
@@ -73,18 +74,28 @@ class RssSource(base.AbstractSource):
         if not doc:
             return []
 
-        min_date_to_load = self._get_min_date_to_load(max_age_load)
-        _LOG.debug("RssSource: src=%d min_date_to_load=%s", self.cfg.oid,
-                   min_date_to_load)
+        min_date_to_load = self._get_min_date_to_load(max_age_load, now)
+        feed_update = _ts2datetime(doc.get('updated_parsed'), now)
+        _LOG.debug("RssSource: src=%d min_date_to_load=%s feed_update=%s",
+                   self.cfg.oid, min_date_to_load, feed_update)
+        if feed_update < min_date_to_load:
+            _LOG.info("RssSource: src=%d feed not update %r < %r",
+                      self.cfg.oid, feed_update, min_date_to_load)
+            return []
 
-        _LOG.debug("get_items: load cache start")
+        # prepare dates etc
+        articles = self._prepare_entries(doc.get('entries'), feed_update)
+        # filter old articles
+        articles = list(self._filter_by_date(articles, min_date_to_load))
+        if not articles:
+            self.cfg.add_log('info', "Not found new articles")
+            return []
+        # cache articles in database
         art_cache = dict(self._get_existing_articles(session))
-        _LOG.debug("get_items: load cache finished items=%d", len(art_cache))
-
-        articles = filter(None,
-                          (self._create_article(
-                              feed, min_date_to_load, art_cache)
-                           for feed in doc.get('entries') or []))
+        # create articles with lookup into cache
+        articles = (self._create_article(feed, art_cache) for feed in articles)
+        # left only new/updated articles
+        articles = filter(None, articles)
 
         _LOG.debug("RssSource: src=%d loaded %d articles",
                    self.cfg.oid, len(articles))
@@ -149,44 +160,57 @@ class RssSource(base.AbstractSource):
             if 'title' in doc.feed:
                 self.cfg.title = doc.feed.title
 
-    def _create_article(self, feed, min_date_to_load, art_cache):
-        published = _ts2datetime(feed.get('published_parsed'))
-        updated = _ts2datetime(feed.get('updated_parsed'))
-        updated = updated or published or datetime.datetime.now()
-        if min_date_to_load and updated < min_date_to_load:
-            _LOG.debug("RssSource: src=%d updated=%s < min_date_to_load=%s",
-                       self.cfg.oid, updated, min_date_to_load)
-            return None
+    # pylint:disable=no-self-use
+    def _prepare_entries(self, entries, feed_updated):
+        for entry in entries:
+            updated = _ts2datetime(entry.get('updated_parsed'))
+            published = _ts2datetime(entry.get('published_parsed'))
+            entry['_published'] = published or feed_updated
+            entry['_updated'] = updated or published or feed_updated
+            yield entry
 
-        internal_id = feed.get('id') or \
-                DBO.Article.compute_id(feed.get('link'),
-                                       feed.get('title'),
-                                       feed.get('author'),
-                                       self.__class__.get_name())
+    def _filter_by_date(self, entries, min_date_to_load):
+        for entry in entries:
+            if min_date_to_load:
+                if entry['_updated'] < min_date_to_load:
+                    _LOG.debug("RssSource: src=%d updated=%s < "
+                               "min_date_to_load=%s",
+                               self.cfg.oid, entry['_updated'],
+                               min_date_to_load)
+                    continue
+            yield entry
+
+    def _create_article(self, entry, art_cache):
+        internal_id = entry.get('id') or \
+                DBO.Article.compute_id(
+                    entry.get('link'), entry.get('title'), entry.get('author'),
+                    self.__class__.get_name())
+
         art = art_cache.get(internal_id)
         if art:
             _LOG.debug("RssSource: src=%d Article already in db: %r",
                        self.cfg.oid, internal_id)
-            if art.updated > updated:
+            if art.updated > entry['_updated']:
                 _LOG.debug("RssSource: src=%d Article is not updated, "
                            "skipping", self.cfg.oid)
                 return None
 
         art = art or DBO.Article()
         art.internal_id = internal_id
-        if 'content' in feed:
-            art.content = feed.content[0].value
+        if 'content' in entry:
+            art.content = entry.content[0].value
         else:
-            art.content = feed.get('value')
-        art.summary = feed.get('summary')
+            art.content = entry.get('value')
+        art.summary = entry.get('summary')
         art.score = self.cfg.initial_score
-        art.title = feed.get('title')
-        art.updated = updated
-        art.published = published or datetime.datetime.now()
-        art.link = feed.get('link')
+        art.title = entry.get('title')
+        art.updated = entry['_updated']
+        art.published = entry['_published']
+        art.link = entry.get('link')
+        art.read = 0
         return art
 
-    def _get_min_date_to_load(self, global_max_age):
+    def _get_min_date_to_load(self, global_max_age, now):
         min_date_to_load = self.cfg.last_refreshed
         max_age_to_load = self.cfg.max_age_to_load
 
@@ -196,8 +220,7 @@ class RssSource(base.AbstractSource):
             return min_date_to_load
 
         if max_age_to_load:  # limit exists
-            limit = datetime.datetime.now() - datetime.timedelta(
-                days=max_age_to_load)
+            limit = now - datetime.timedelta(days=max_age_to_load)
             if min_date_to_load:
                 min_date_to_load = max(limit, min_date_to_load)
             else:
