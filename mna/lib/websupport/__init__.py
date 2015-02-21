4bf292d15f50e8e3bc9a6db47d87fe488b797478
Karol Będkowski <karol.bedkowski@gmail.com>
2015-02-21 14:03:52 +0100

    support requests library

--- mna/lib/websupport/__init__.py
+++ mna/lib/websupport/__init__.py
@@ -7,80 +7,23 @@ __author__ = "Karol Będkowski"
 __copyright__ = "Copyright (c) Karol Będkowski, 2014-2015"
 __version__ = "2015-01-18"
 
-import urllib2
-import time
-import datetime
 import logging
 import itertools
+import urllib2
 import os.path
 
 from lxml import etree
 
+from .errors import LoadPageError
 
 _LOG = logging.getLogger(__name__)
 
-
-class LoadPageError(RuntimeError):
-    pass
-
-
-# pylint:disable=no-init
-class _DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
-    def http_error_default(self, req, fp, code, msg, headers):
-        result = urllib2.HTTPError(
-            req.get_full_url(), code, msg, headers, fp)
-        result.status = code
-        return result
-
-
-def download_page(url, etag=None, modified=None):
-    """ Download Page from `url` with optional last etag and modified time.
-        When no updates return code 304 and no content.
-
-    Args:
-        url (str): address object to download
-        etag (str): optional previous etag
-        modified (str): optional previous modification date
-
-    Return:
-        (page headers, page content)
-        Custom page headers:
-            _encoding - page encoding
-            _status - connection status code
-            _modified - last modified date
-            _url - page url
-    """
-    request = urllib2.Request(url)
-    opener = urllib2.build_opener(_DefaultErrorHandler())
-    if modified:
-        request.add_header('If-Modified-Since', modified)
-    if etag:
-        request.add_header('If-None-Match', etag)
-    request.add_header(
-        'User-Agent',
-        'Mozilla/5.0 (X11; Linux i686; rv:36.0) Gecko/20100101 Firefox/36.0')
-    try:
-        conn = opener.open(request, timeout=10)
-        info = dict(conn.headers)
-        info['_status'] = conn.code
-        info['_url'] = url
-        info['_encoding'] = conn.headers.getencoding()
-        if info['_encoding'] == '7bit':
-            info['_encoding'] = None
-        modified = conn.headers.getdate('Last-Modified') or \
-                conn.headers.getdate('last-modified')
-        info['_modified'] = modified
-        info['_last_modified'] = \
-                datetime.datetime.fromtimestamp(time.mktime(modified)) \
-                if modified else None
-        if conn.code == 200:
-            content = conn.read()
-            return info, content
-        elif conn.code == 304:  # not modified
-            return info, None
-        raise LoadPageError("%d: %s" % (conn.status, conn.reason))
-    except urllib2.URLError, err:
-        raise LoadPageError(err)
+try:
+    from ._requests import download_page
+    _LOG.info('websupport using python-requests')
+except ImportError:
+    from ._raw import download_page
+    _LOG.info('websupport using urllib2')
 
 
 def _parse_html_content(content, encoding=None):
